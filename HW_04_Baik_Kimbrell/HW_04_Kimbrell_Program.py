55cb5bccb55c506058a517639dfdf272feb5a59a
Caitlin Kimbrell <cbk3451@g.rit.edu>
2020-09-23 20:47:33 -0400

    finishheeeddd just need comments

---
+++ HW_04_Baik_Kimbrell/HW_04_Kimbrell_Program.py
@@ -0,0 +1,63 @@
+'''
+@author Caitlin Kimbrell
+doing function #424
+lets get it
+'''
+import BirdBathFunction_424_v420 as bb424
+import BirdBathFunction_431_v420 as bb431
+import math
+import numpy as np
+
+def helper(func):
+    curr_best_water = -1
+    curr_best_roll = -1
+    curr_best_twist = -1
+    curr_best_tilt = -1
+    mini = -45
+    maxi = 45
+    delta = [12, 6, 1]
+    for d in delta:
+        roll = mini
+        tilt = mini
+        twist = mini
+        curr_best_water, curr_best_roll, curr_best_tilt, curr_best_twist = findBest(roll, tilt, twist, mini, maxi, d, func)
+        curr_bests = [curr_best_roll, curr_best_tilt, curr_best_twist]
+        mini = min(curr_bests) - d
+        maxi = max(curr_bests) + d
+
+    return curr_best_water, curr_best_roll, curr_best_tilt, curr_best_twist
+
+
+def findBest(roll, tilt, twist, min, max, delta, func):
+    best_water = -1
+    best_roll = -1
+    best_tilt = -1
+    best_twist = -1
+    while roll < max:
+        tilt = min
+        while tilt < max:
+            twist = min
+            while twist < max:
+                water = func(roll, tilt, twist)
+                if(water > best_water):
+                    best_water = water
+                    best_roll = roll
+                    best_tilt = tilt
+                    best_twist = twist
+                twist+= delta
+            tilt +=  delta
+        roll += delta
+    return best_water, best_roll, best_tilt, best_twist
+
+
+if __name__ == '__main__' :
+    print('Bird Bath Function 424\n')
+    best_water, best_roll, best_tilt, best_twist = helper(bb424.BirdbathFunc424)
+    print('Best Fraction of Water = ', best_water, '\n')
+    print('Roll: ', best_roll, " Tilt: ", best_tilt, " Twist: ", best_twist)
+    print('\n###\n')
+    print('Bird Bath Function 431\n')
+    best_water, best_roll, best_tilt, best_twist = helper(bb431.BirdbathFunc431)
+    print('Best Fraction of Water = ', best_water, '\n')
+    print('Roll: ', best_roll, " Tilt: ", best_tilt, " Twist: ", best_twist)
+
