1510dde31d85a5c76a5d6fbcfb66244a46890f53
JiWoo Baik <jbaik@resilientvitality.com>
2020-11-19 22:00:42 -0500

    data reading in

--- PROJECT_GPS_Baik_Kimbrell/GPS_to_KML.py
+++ PROJECT_GPS_Baik_Kimbrell/GPS_to_KML.py
@@ -3,6 +3,7 @@ GPS_to_KML.py reads GPS file and converts to KML file that can be viewed on Goog
 """
 import sys
 import pandas as pd
+import csv
 
 def preprocess(gps_data):
     print(gps_data.head(10))
@@ -14,8 +15,12 @@ def eliminate_unnecessary_data(gps_data):
     print(filtered_data.head(10))
 
 def main():
-    print(sys.argv[1])
-    gps_df = pd.read_csv(sys.argv[1])    # read in the GPS file
+    gps_df = pd.DataFrame(columns=['Type', 'UTC', 'Status', 'Latitude', 'N/S of Longitude', ' Longitude', 'E/W of Longitude', 'Speed in knots', 'Track', 'Fix UTC', 'Degree (Magnetic variation)', 'E/W Magnetic Variation', 'Mode Indicator', 'Checksum'])
+    with open(sys.argv[1], 'rt') as readhandle:     # read in the GPS file
+        reader = csv.reader(readhandle)
+        for row in reader:
+            if row[0] == '$GPRMC':
+
     preprocess(gps_df)
     eliminate_unnecessary_data(gps_df)
 
